f576cbc75843d375bc80e2f811356d4d65ae01be
Sn0wsurfer <41308117+Sn0wsurfer@users.noreply.github.com>
2023-07-18 20:41:16 +1200

    Update web_serverR1.py
    replace decimal with float

--- web_serverR1.py
+++ web_serverR1.py
@@ -3,11 +3,18 @@ import socket
 from time import sleep
 from picozero import pico_temp_sensor, pico_led
 import machine
-ssid = '******'
-password = '******'
+import urequests as requests
+#import decimal
+import ujson as json
+
+# network.hostname("****")
+ssid = '****'
+password = '****'
+Hname = "****"
 
 def connect():
     #Connect to WLAN
+    wlan = network.hostname(Hname)
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
@@ -67,10 +74,40 @@ def serve(connection):
         html = webpage(temperature, state)
         client.send(html)        
         client.close()
+        
+def openweather():
+#     data = requests.get("https://api.openweathermap.org/data/2.5/weather?q=wellington,nz&appid=7d635aea345b49cbecc4d2158bebe7b2").json()
+    data = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=-41.328&lon=174.805&appid=7d635aea345b49cbecc4d2158bebe7b2").json()
+    json_str = json.dumps(data)
+    resp = json.loads(json_str)
+    data2 = data.get("wind")
+    json_str2 = json.dumps(data2)
+    resp = json.loads(json_str2)
+    WindSpeed = float
+    WindSpeed = (resp["speed"])
+    Windno = int(round(WindSpeed)*3.6)
     
+    data3 = data.get("main")
+    json_str3 = json.dumps(data3)
+    resp = json.loads(json_str3)
+    temptx = float
+    temptx = (resp["temp"])
+    temp = int(round(temptx -273.15))
+    # Lightno = 90
+    #test print varible if required
+    Stemp = "Temperature " + str(temp)
+    SWindno = "wind Speed " + str(Windno)
+    print (Stemp)
+    print (SWindno)
+       
 try:
     ip = connect()
     connection = open_socket(ip)
+    openweather()
     serve(connection)
+
+    
 except KeyboardInterrupt:
-    machine.reset()        
+    machine.reset()
+    
+
